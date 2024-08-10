5a1b5350ebcd9b277b9f42686b2af12f3377ad16
Jason Xiang <xiangchuhang@gmail.com>
2024-08-09 20:39:39 -0400

    black background

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,10 +14,35 @@ const config = {
     extend: {
       backgroundColor: {
         'custom-black': '#000000',
+        'default': '#000000', // Set default background to black
+      },
+      textColor: {
+        'default': '#ffffff', // Set default text color to white
+      },
+      colors: {
+        // Define a custom color palette for dark mode
+        dark: {
+          100: '#f5f5f5',
+          200: '#e5e5e5',
+          300: '#d4d4d4',
+          400: '#a3a3a3',
+          500: '#737373',
+          600: '#525252',
+          700: '#404040',
+          800: '#262626',
+          900: '#171717',
+        },
       },
     },
   },
+  // Enable dark mode
+  darkMode: 'class',
   plugins: [
+    function ({ addBase, theme }) {
+      addBase({
+        'body': { backgroundColor: theme('colors.black'), color: theme('colors.white') },
+      })
+    },
     function ({ matchUtilities, theme }) {
       matchUtilities(
         {
