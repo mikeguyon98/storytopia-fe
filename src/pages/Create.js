532d81b3b6f202c5f6d6782ac36dec7557f33a91
Jason Xiang <xiangchuhang@gmail.com>
2024-08-14 01:58:27 -0400

    set disability optional

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -143,7 +143,7 @@ const Create = () => {
                   id="disability"
                   name="disability"
                   autoComplete="disability"
-                  required
+                  optional
                   className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 px-3 resize-y overflow-y-auto h-12"
                   placeholder="Optionally enter any disability you want considered"
                   value={disabilities}
